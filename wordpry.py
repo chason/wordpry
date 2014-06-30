6493776e1c315b09111e48d3692ef11fe4a8246b
Chason Chaffin <cchaffin@mediatemple.net>
2014-06-30 12:46:39 -0700

    Have working _login function

--- wordpry.py
+++ wordpry.py
@@ -3,6 +3,11 @@ import requests
 
 __version__ = 0.1
 
+
+class LoginException(Exception):
+    pass
+
+
 class WordPry(object):
     """
     WordPry
@@ -10,10 +15,17 @@ class WordPry(object):
     Class to facilitate manipulating WordPress when we don't have a better way.
     """
 
-    def init(self, log, pwd, website, ftp_user, ftp_pwd):
+    def __init__(self, log, pwd, website, ftp_user=None, ftp_pwd=None):
         """
         Creates object and logs into WordPress site, then stores the session.
+        :type self: object
+        :param log:
+        :param pwd:
+        :param website:
+        :param ftp_user:
+        :param ftp_pwd:
         """
+
         self.log = log
         self.pwd = pwd
         self.website = website
@@ -21,7 +33,7 @@ class WordPry(object):
         self.login_url = None
         self.admin_url = website + "/wp-admin"
 
-        self._session = self._login(log, pwd, website)
+        self._session = self._login()
 
     def list_plugins(self):
         """
@@ -37,6 +49,7 @@ class WordPry(object):
         returns plugin info
         """
         pass
+
     def activate_plugin(self, plugin):
         """
         Activates plugin.
@@ -45,8 +58,44 @@ class WordPry(object):
         """
         pass
 
-    def _login(self, log, pwd, website):
-        s = requests.Session()
+    def _login(self):
+        try:
+            s = self._session
+        except AttributeError:
+            s = requests.Session()
+
+        # Lets get the correct login url
+        r = s.get(self.admin_url)
+        self.login_url = r.url
+
+        # Setup the headers so the server thinks we're a real browser
+        headers = {'Origin': self.website,
+                   'Referer': self.admin_url,
+                   'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0',
+                   'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                   'Connection': 'Keep-Alive',
+        }
+        s.headers.update(headers)
+
+        # Construct payload and POST to login_url
+        payload = {'log': self.log,
+                   'pwd': self.pwd,
+                   'wp-submit': 'Log In',
+                   'redirect_to': self.admin_url,
+                   'testcookie': 1,
+                   'rememberme': 'forever',
+        }
+        r = s.post(self.login_url, data=payload)
+        if r.status_code != 200:
+            raise LoginException(
+                "Server returned incorrect status code: {}".format(r.status_code))
+
+        auth_cookie = filter(lambda x: x.name.startswith('wordpress_logged_in'),
+                             s.cookies)
+        if not auth_cookie:
+            raise LoginException("No valid authorization cookie from WordPress")
+
+        return s
 
     def _upload_plugin_ftp(self):
         pass
