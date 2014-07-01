926aaafc827a36c128e4164e963cf8054c26ada3
Chason Chaffin <cchaffin@mediatemple.net>
2014-07-01 16:27:12 -0700

    Starting on plugin install code

--- wordpry.py
+++ wordpry.py
@@ -1,5 +1,7 @@
 from __future__ import print_function
 import requests
+import lxml.html
+from collections import namedtuple
 
 __version__ = 0.1
 
@@ -7,6 +9,8 @@ __version__ = 0.1
 class LoginException(Exception):
     pass
 
+
+Plugin = namedtuple('Plugin', ['name', 'version', 'active'])
 
 class WordPry(object):
     """
@@ -30,8 +34,12 @@ class WordPry(object):
         self.pwd = pwd
         self.website = website
 
+        self.ftp_user = ftp_user
+        self.ftp_pwd = ftp_pwd
+
         self.login_url = None
         self.admin_url = website + "/wp-admin"
+        self.plugin_url = self.admin_url + "/plugins.php"
 
         self._session = self._login()
 
@@ -40,7 +48,15 @@ class WordPry(object):
         Lists WordPress plugins and their version.
         Returns list of plugins
         """
-        pass
+        plugin_list = []
+        plugin_page = self._get_page(self.plugin_url)
+        raw_list = plugin_page.get_element_by_id('the-list')
+        for elem in raw_list.getchildren():
+            name = elem.attrib['id']
+            if elem.attrib['class'] == 'active':
+                active = True
+
+            plugin_list.append(Plugin(name=name, active=active))
 
     def install_plugin(self, plugin):
         """
@@ -58,7 +74,19 @@ class WordPry(object):
         """
         pass
 
+    def deactivate_plugin(self, plugin):
+        """
+        Deactivates plugin.
+        ``plugin`` should be identifier for plugin from list_plugins
+        returns boolean
+        """
+        pass
+
     def _login(self):
+        """
+        :return: requests.Session
+        :raise LoginException:
+        """
         try:
             s = self._session
         except AttributeError:
@@ -74,7 +102,7 @@ class WordPry(object):
                    'User-agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:30.0) Gecko/20100101 Firefox/30.0',
                    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
                    'Connection': 'Keep-Alive',
-        }
+                   }
         s.headers.update(headers)
 
         # Construct payload and POST to login_url
@@ -88,7 +116,8 @@ class WordPry(object):
         r = s.post(self.login_url, data=payload)
         if r.status_code != 200:
             raise LoginException(
-                "Server returned incorrect status code: {}".format(r.status_code))
+                "Server returned incorrect status code: {}".format(
+                    r.status_code))
 
         auth_cookie = filter(lambda x: x.name.startswith('wordpress_logged_in'),
                              s.cookies)
@@ -102,3 +131,9 @@ class WordPry(object):
 
     def _upload_plugin_http(self):
         pass
+
+    def _get_page(self, page):
+        s = self._session
+        r = s.get(page)
+
+        return lxml.html.fromstring(r.content)
